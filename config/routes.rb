dd68da1fd48cfd4a92e546823260e0c189ffa012
Skye <skye.weir@gmail.com>
2016-09-12 12:34:20 -0500

    forcing authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :records
+
+  root 'home#index'
+
   devise_for :users
-  get 'home/index'
+
+  resources :categories
+
+  resources :records
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
